60eb487a7cd6d51a451bfac7df96660c3ed00441
dibley1973 <dibley1973@yahoo.co.uk>
2014-03-28 05:51:23 +0000

    20140328-0551-DWDT Initial code commit

---
+++ Dibware.UnitTesting/IntegerAssert.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Dibware.UnitTesting
+{
+    public static class IntegerAssert
+    {
+        #region IsLessThan
+
+        /// <summary>
+        /// Asserts that short integer value1 is less than value2.
+        /// </summary>
+        /// <param name="value1">The value1.</param>
+        /// <param name="value2">The value2.</param>
+        public static void IsLessThan(Int16 value1, Int16 value2)
+        {
+            Assert.IsTrue(value1 < value2);
+        }
+
+        /// <summary>
+        /// Asserts that the integer value1 is less than value2.
+        /// </summary>
+        /// <param name="value1">The value1.</param>
+        /// <param name="value2">The value2.</param>
+        public static void IsLessThan(Int32 value1, Int32 value2)
+        {
+            Assert.IsTrue(value1 < value2);
+        }
+
+        /// <summary>
+        /// Asserts that the long integer value1 is less than value2.
+        /// </summary>
+        /// <param name="value1">The value1.</param>
+        /// <param name="value2">The value2.</param>
+        public static void IsLessThan(Int64 value1, Int64 value2)
+        {
+            Assert.IsTrue(value1 < value2);
+        }
+
+        #endregion
+    }
+}
---
+++ Dibware.UnitTesting/StringAssert.cs
@@ -0,0 +1,48 @@
+﻿using Dibware.UnitTesting.Resources;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Dibware.UnitTesting
+{
+    /// <summary>
+    /// Encapsulates String based asserts
+    /// Ref:
+    ///   http://geekswithblogs.net/sdorman/archive/2009/01/31/adding-custom-assertions-to-mstest.aspx
+    /// </summary>
+    public static class StringAssert
+    {
+        #region IsEmpty
+
+        /// <summary>
+        /// Asserts that the specified value is empty.
+        /// </summary>
+        /// <param name="value">The value to be tested.</param>
+        public static void IsEmpty(String value)
+        {
+            IsEmpty(value, AssertFailureMessages.Generic, String.Empty, value);
+        }
+
+        /// <summary>
+        /// Asserts that the specified value is empty.
+        /// </summary>
+        /// <param name="value">The value to be tested.</param>
+        /// <param name="message">A message to display. This message can be seen in the unit test results.</param>
+        public static void IsEmpty(String value, String message)
+        {
+            IsEmpty(value, message, null);
+        }
+
+        /// <summary>
+        /// Asserts that the specified value is empty.
+        /// </summary>
+        /// <param name="value">The value to be tested.</param>
+        /// <param name="message">A message to display. This message can be seen in the unit test results.</param>
+        /// <param name="parameters">An array of parameters to use when formatting <paramref name="message"/>.</param>
+        public static void IsEmpty(String value, String message, params object[] parameters)
+        {
+            Assert.IsTrue(value.Length == 0, message, parameters);
+        }
+
+        #endregion
+    }
+}
